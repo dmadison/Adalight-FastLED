65e26ed88fb4a3dff61217349c231f7a3396673c
David Madison <dmadison@users.noreply.github.com>
2017-05-05 09:18:27 -0400

    Updated date

--- Arduino/LEDstream_FastLED/LEDstream_FastLED.ino
+++ Arduino/LEDstream_FastLED/LEDstream_FastLED.ino
@@ -4,7 +4,7 @@
  * library (http://fastled.io) for driving led strips.
  * 
  * http://github.com/dmadison/Adalight-FastLED
- * Last Updated: 2017-04-23
+ * Last Updated: 2017-05-05
  */
 
 // --- General Settings
